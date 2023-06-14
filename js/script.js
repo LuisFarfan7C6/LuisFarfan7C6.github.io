a400c01afb96add199fcb88b781a2e5867b73cbd
Luis Farfán <131408743+LuisFarfan7C6@users.noreply.github.com>
2023-06-14 14:15:10 -0500

    script.js

--- js/script.js
+++ js/script.js
@@ -53,12 +53,13 @@ window.onscroll = () => {
 
     $form.addEventListener('submit', handleSubmit)
 
-    function handleSubmit(click) {
-        click.preventDefault()
+    function handleSubmit(onclick) {
+        onclick.preventDefault()
         const form = new FormData(this)
         $buttonMail.setAttribute('href', `mailto:luis.farfan.680@gmail.com?subject=${form.get('subject')}&body=Hola, mi nombre es ${form.get('name')} y mi teléfono es ${form.get('number')}.%0A%0A${form.get('body')}`)
         $buttonMail.click()
-        alert('Gracias por escribir, te responderé a la brevedad')
         this.reset();
+        alert('Gracias por escribir, te responderé a la brevedad')        
+        location.reload()
     }
 }
